74e5a76cbc88fb68e68eceea6072e57d98aa7c50
JulienDrochon <41402969+JulienDrochon@users.noreply.github.com>
2021-01-13 10:04:44 +0100

    Update C_B_B_F_Malette01_3servo.ino

--- C_B_Actuators/C_B_B_Servomototor/C_B_B_F_Malette01_3servo/C_B_B_F_Malette01_3servo.ino
+++ C_B_Actuators/C_B_B_Servomototor/C_B_B_F_Malette01_3servo/C_B_B_F_Malette01_3servo.ino
@@ -9,8 +9,8 @@ int pos = 0;    // variable pour mémoriser la position du moteur
 
 void setup() {
   myservo.attach(3);  // fixe le servo sur la broche 3 à l'objet servo
-  myservo.attach(5);  // fixe le servo sur la broche 3 à l'objet servo
-  myservo.attach(6);  // fixe le servo sur la broche 3 à l'objet servo
+  myservo.attach(5);  // fixe le servo sur la broche 5 à l'objet servo
+  myservo.attach(6);  // fixe le servo sur la broche 6 à l'objet servo
 }
 
 void loop() {
@@ -37,11 +37,11 @@ void loop() {
   //3e moteur (broche 6)
   for (pos = 0; pos <= 180; pos += 1) { // va de 0 degrés à 180 degrés
     // in steps of 1 degree
-    myservo03.write(pos);              // dire au 2e servo d'aller en position dans la variable "pos"
+    myservo03.write(pos);              // dire au 3e servo d'aller en position dans la variable "pos"
     delay(15);                       // attend 15 ms que le servo atteigne la position
   }
   for (pos = 180; pos >= 0; pos -= 1) { // passe de 180 degrés à 0 degré
-    myservo03.write(pos);              // dire au 2e servo d'aller en position dans la variable "pos"
+    myservo03.write(pos);              // dire au 3e servo d'aller en position dans la variable "pos"
     delay(15);                       // attend 15 ms que le servo atteigne la position
   }
 }
